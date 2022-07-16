722179897b02358a43843c409c4589204e5ecc9b
Cristian Rosca <cristianrrosca@gmail.com>
2022-07-16 16:29:22 +0200

    complete app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { ethers } from "ethers";
 
-
 function App() {
-
-  const {greet, setGreet} = useState('');
-  const [balance, setBalance] = useState();
-  const [greetingValue, setGreetingValue] = useState(''); // note to use [], not {}
   const [depositValue, setDepositValue] = useState(0);
-
-
+  const [greet, setGreet] = useState('');
+  const [greetingValue, setGreetingValue] = useState('');
+  const [balance, setBalance] = useState();
 
   const provider = new ethers.providers.Web3Provider(window.ethereum)
   const signer = provider.getSigner()
-  const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-
-  // import the contract abi 
-  const abi = [
+  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+  const ABI = [
     {
       "inputs": [
         {
@@ -61,76 +55,86 @@ function App() {
       "stateMutability": "nonpayable",
       "type": "function"
     }
-  ]
-
-  // create contract object 
-  const contract = new ethers.Contract(contractAddress, abi, signer);
+  ];
+  const contract = new ethers.Contract(contractAddress, ABI, signer);  
 
   useEffect(() => {
-    const connectWallet = async () => {
-      provider.send("eth_requestAccounts", []);
+    const requestAccounts = async () => {
+      await provider.send("eth_requestAccounts", []);
+    }
+    
+    const getGreeting = async () => {
+      const greeting = await contract.greet();
+      setGreet(greeting);
     }
 
     const getBalance = async () => {
-      const balance = await provider.getBalance(contractAddress)
-      const balanceFormatted = ethers.utils.formatEther(balance)
-      setBalance(balanceFormatted)
+      const balance = await provider.getBalance(contractAddress);
+      setBalance(ethers.utils.formatEther(balance));
     }
 
-    connectWallet()
+    requestAccounts()
       .catch(console.error)
-
     getBalance()
       .catch(console.error)
-  })
-  
-  
-  
+    getGreeting()
+      .catch(console.error)
+  }, [])
 
   const handleDepositChange = (e) => {
     setDepositValue(e.target.value)
   }
-  
+
   const handleGreetingChange = (e) => {
     setGreetingValue(e.target.value);
   }
 
-  const handleDepositSubmit = (e) => { // 'e' for event
-    e.preventDefault(); // prevents from form default being submitted and auto-refreshing the page
-    console.log(depositValue);
+  const handleDepositSubmit = async (e) => {
+    e.preventDefault();
+    const ethValue = ethers.utils.parseEther(depositValue)
+    const deposit = await contract.deposit({value: ethValue});
+    await deposit.wait();
+    const balance = await provider.getBalance(contractAddress);
+    setBalance(ethers.utils.formatEther(balance));
   }
 
-  const handleGreetingSubmit = (e) => {
+  const handleGreetingSubmit = async (e) => {
     e.preventDefault();
-    console.log(greetingValue);
+    await contract.setGreeting(greetingValue)
+    setGreet(greetingValue);
+    setGreetingValue('');
   }
 
   return (
     <div className="container">
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col">
-            <h3>Greetings</h3>
-            <p>Contract Balance: {balance}</p>
-          </div>
-          <div className="col">
-          <form onSubmit={handleDepositSubmit}>
+      <div className="row mt-5">
+
+        <div className="col">
+          <h3>{greet}</h3>
+          <p>Contract Balance: {balance} ETH</p>
+        </div>
+
+        <div className="col">
+          <div className="mb-3">
+            <h4>Deposit ETH</h4>
+            <form onSubmit={handleDepositSubmit}>
               <div className="mb-3">
-                <input type="number" className="form-control" placeholder = "0" onChange={handleDepositChange} value={depositValue}/>
+                <input type="number" className="form-control" placeholder="0" onChange={handleDepositChange} value={depositValue} />
               </div>
               <button type="submit" className="btn btn-success">Deposit</button>
-          </form>
-          <form className="mt-5" onSubmit={handleGreetingSubmit}>
+            </form>
+
+            <h4 className="mt-3">Change Greeting</h4>
+            <form onSubmit={handleGreetingSubmit}>
               <div className="mb-3">
-                <input type="text" className="form-control"  onChange={handleGreetingChange} value={greetingValue}/>
+                <input type="text" className="form-control" placeholder="" onChange={handleGreetingChange} value={greetingValue} />
               </div>
               <button type="submit" className="btn btn-dark">Change</button>
-          </form>
+            </form>
           </div>
-          
         </div>
+      </div>
     </div>
-</div>
   );
 }
 
